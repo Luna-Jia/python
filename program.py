aa5b2751c1a47a490a0e6dd2fa86b00bf3c8dfb4
Meng Luna Jia <73403516+Luna-Jia@users.noreply.github.com>
2023-10-03 13:59:43 -0400

    student score max/min/average

--- program.py
+++ program.py
@@ -68,25 +68,91 @@ def display_primes(n):
             print(i)
 
 
+# 4.
+# Write a function process_scores( )that reads student names and their scores from a user and displays the following: 
+# the average score, the minimum score, the maximum score, and the students who received the minimum and maximum scores. 
+# The function repeatedly prompts the user to enter the name and the score of a student. The user enters the name and the 
+# score separated by blank in one line. The name is a single word and the score is an integer. The user enters q in lower 
+# or upper case to quit. The user does not enter score after q. The outputs are displayed with appropriate messages.
+
+def process_scores():
+    
+    # define and initiate two empty list as containers for name and score
+    name = []
+    score = []
+
+    # keep prompting user for input until quit
+    while True:
+        
+        # collect user's input in a string varibale called "enter"
+        enter = input("Enter student's name and score. Enter q or Q to quit: ")
+        # if user enter q or Q, quit
+        if enter.lower() == "q":break
+
+        # split string "enter" with a pace, result "studentScore" is a list
+        studentScore = enter.split()
+
+        # append name to name list
+        name.append(studentScore[0])
+        # append score to score list; change score type from string in integer
+        score.append(int(studentScore[1]))
+
+    #outside the while loop, initialize and define a new variable called sum
+    sum = 0
+    # calculate sum and average 
+    for element in score:
+        sum = sum + element
+        average = sum / len(score)
+
+    # use list function for min and max
+    minScore = min(score)
+    maxScore = max(score)
+
+    # traverse the list to find the name with min score
+    for i in range (len(name)):
+        if(score[i] == minScore):
+            print("Minimum score: " + name[i] + " " + str(score[i]) )
+
+    # traverse the list to find the name with max score    
+    for i in range (len(name)):
+        if(score[i] == maxScore):
+            print("Maximum score: " + name[i] + " " + str(score[i]))
+
+    print("Average score: " + str(average))
+
+
 # 10. Question Menu
 def main():
-    # prompte the user for input, then save it to variable questionNum
-    questionNum = int(input("Enter a question number from 1 ~ 9 (Enter 0 to quit):"))
+    print("Question 1: compute pi")
+    print("Question 2: compute square root")
+    print("Question 3: display prime number")
+    print("Question 4: student score")
 
-    if (questionNum == 1):
-        num = int(input("Enter a integer term number to compute pi: "))
-        # call function 
-        compute_pi(num)
 
-    elif (questionNum == 2):
-        num = int(input("Enter a number to compute square root: "))
-        # call function 
-        compute_sqrt(num)
+    # continue prompte, until user quit
+    while True:
+        # prompte the user for input, then save it to variable questionNum
+        questionNum = int(input("Enter a question number from 1 ~ 9 (Enter 0 to quit):"))
+
+        if(questionNum == "0"): break
+
+        if (questionNum == 1):
+            num = int(input("Enter a integer term number to compute pi: "))
+            # call function 
+            compute_pi(num)
+
+        elif (questionNum == 2):
+            num = int(input("Enter a number to compute square root: "))
+            # call function 
+            compute_sqrt(num)
     
-    elif( questionNum == 3):
-        num = int(input("Enter a number to display all prime: "))
-        # call function 
-        display_primes(num)
+        elif( questionNum == 3):
+            num = int(input("Enter a number to display all prime: "))
+            # call function 
+            display_primes(num)
+    
+        elif(questionNum == 4):
+            process_scores()
 
 
 
